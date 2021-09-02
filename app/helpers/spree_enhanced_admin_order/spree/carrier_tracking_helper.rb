f7c475342c43e1f4f83f32b3492d1e85a97de24c
JX <johnnyxiang2017@gmail.com>
2021-09-02 10:45:54 -0700

    assign carrier based on tracking number if its not provided

--- app/helpers/spree_enhanced_admin_order/spree/carrier_tracking_helper.rb
+++ app/helpers/spree_enhanced_admin_order/spree/carrier_tracking_helper.rb
@@ -17,6 +17,32 @@ module SpreeEnhancedAdminOrder
           content_tag(:span, tracking_text)
         end
       end
+
+
+      def get_carrier(tracking_number)
+        tracking_number_str = tracking_number.downcase
+        if tracking_number_str.start_with?("gm")
+          return "DHLGlobalMail"
+        end
+        if tracking_number.first == "9" and tracking_number.length > 15
+          return "USPS"
+        end
+        if is_number?(tracking_number) and tracking_number.length == 10
+          return "DHL"
+        end
+        if is_number?(tracking_number) and tracking_number.length == 12
+          return "FedEx"
+        end
+        if tracking_number_str.start_with?('1z') and tracking_number_str.length == 18
+          return 'UPS'
+        end
+
+        "USPS"
+      end
+
+      def is_number? string
+        true if Float(string) rescue false
+      end
     end
   end
 end
--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -1,5 +1,17 @@
 module Spree
   module ShipmentDecorator
+    def self.prepended(base)
+      base.include SpreeEnhancedAdminOrder::Spree::CarrierTrackingHelper
+      base.before_update :check_and_update_carrier
+    end
+
+    def check_and_update_carrier
+      if self.carrier.blank? and self.tracking.present?
+        self.carrier = get_carrier(self.tracking)
+      end
+    end
+
+
     def tracking_link
       if self.carrier.blank? or self.tracking.blank?
         return self.tracking_url
